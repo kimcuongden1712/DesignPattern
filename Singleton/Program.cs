cd0ad6ac7068a0332dd753aec57158893897d7a7
anhttran <anhttran@kms-technology.com>
2022-09-20 17:41:13 +0700

    commit to origin

---
+++ Singleton/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SingletonPattern;
+
+Console.WriteLine("=======Single thread=============");
+Singleton.Instance().SayHi();
+
+Console.WriteLine("=======Thread=============");
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance = new Singleton();
+
+        private Singleton()
+        { }
+
+        public static Singleton Instance() { return _instance; }
+
+        public void SayHi() 
+        {
+            Console.WriteLine("Singleton Hi");
+        }
+    }
+}
